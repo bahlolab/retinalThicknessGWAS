52331de4255217ca1ddd58de365f5300c3769ee5
Vicki Jackson <jackson.v@wehi.edu.au>
2024-04-09 17:29:13 +1000

    putting together sensitivity analyses 2024-04-09

--- sensitivityAnalyses/01_generateGWASInput.R
+++ sensitivityAnalyses/01_generateGWASInput.R
@@ -32,6 +32,44 @@ surgery <- fread("/stornext/Bioinf/data/lab_bahlo/projects/misc/UKBiobank/data/a
 
 hadSurgeryDefinite <- surgery[glaucomaSurgeryDefinite == 1, f.eid]
 
+## make smoking pheno file
+smokeDir <- "/stornext/Bioinf/data/lab_bahlo/projects/misc/UKBiobank/data/app28541/phenoData/smokingStatusIDs"
+smoke<- data.table(f.eid = scansDTlinked[,patID])
+
+smokeInstances <- lapply(c(0,1), function(instance) {
+
+instanceDT <- lapply(c("current","previous", "never"), function(status) {
+  
+  dt<- paste0(smokeDir,"/",status,"Smoke_",instance,".csv") %>%
+  fread %>%
+  setnames(., c("f.eid")) %>%
+  .[, status := status]
+
+return(dt)  
+}) %>%
+rbindlist %>%
+setnames(., "status", paste0("smokingStatus_",instance))
+
+})
+
+## merge smokeInstances with smoke, on f.eid
+## recode to factor for each instance to never=0, previous=1, current=2
+smoke <-    merge(smoke, smokeInstances[[1]], by = "f.eid", all.x = TRUE) %>%
+  merge(., smokeInstances[[2]], by = "f.eid", all.x = TRUE) %>%
+  .[, smokingFactor_0 := case_when(smokingStatus_0 == "never" ~ 0,
+                                 smokingStatus_0 == "previous" ~ 1,
+                                 smokingStatus_0 == "current" ~ 2 )] %>%
+  .[, smokingFactor_1 := case_when(smokingStatus_1 == "never" ~ 0,
+                                  smokingStatus_1 == "previous" ~ 1,
+                                  smokingStatus_1 == "current" ~ 2 )] 
+
+
+
+
+
+
+
+
 file <- paste0(dataDir,"ukb41258.tab")
 names <- fread(file, nrows = 0) %>%
   names
@@ -42,18 +80,21 @@ getIdx <- function(code) {
 
 cols <- getIdx(c("f.eid", "f.50.0.0", "f.50.1.0"))
 
-covs <- fread(file, select = cols)
+covs <- fread(file, select = cols) %>%
+.[smoke[, .(f.eid, smokingFactor_0, smokingFactor_1)], on = f.eid]
 
 pcs <- paste0(dataDir,"ukb32825.tab") %>%
   fread(., select = c(1, 1145:1154)) %>%
   setnames(. ,c("patIDhda", paste0("PC",c(1:10))))
 
-covsOut <- scansDTlinked[, c("patIDhda", "patID", "visit", "eye", "sex", "age", "device", "meanRefErr")] %>%
+covsFull <- scansDTlinked[, c("patIDhda", "patID", "visit", "eye", "sex", "age", "device", "meanRefErr")] %>%
   covs[., on = c(f.eid = "patID")] %>%
   pcs[., on = "patIDhda"] %>%
   .[, standHeight := case_when(visit == 0 ~ f.50.0.0,
                       visit == 1 ~ f.50.1.0)] %>%
   .[, ageSq := age^2] %>%
+  .[, smokingStatus := case_when(visit == 0 ~ smokingFactor_0,
+                                visit == 1 ~ smokingFactor_1)] %>%
   # fastDummies::dummy_cols(., select_columns = c("eye", "device")) %>%
   .[, deviceCat := paste0("dev",device)] %>%
   cbind(data.table(FID = .[,patIDhda], IID = .[,patIDhda]), .) %>%
@@ -61,7 +102,7 @@ covsOut <- scansDTlinked[, c("patIDhda", "patID", "visit", "eye", "sex", "age", 
   .[!is.na(IID)]
 
 
-phenoOut <- scansDTlinked[, !c("patID", "visit", "eye", "sex", "age", "device", "meanRefErr")] %>%
+phenoFull <- scansDTlinked[, !c("patID", "visit", "eye", "sex", "age", "device", "meanRefErr")] %>%
   cbind(data.table(FID = .[,patIDhda], IID = .[,patIDhda]), .) %>%
   .[, patIDhda := NULL]
 
@@ -69,11 +110,11 @@ phenoOut <- scansDTlinked[, !c("patID", "visit", "eye", "sex", "age", "device", 
 pixels <- data.table(pixel = names(phenoOut)[!names(phenoOut) %in% c("FID", "IID")]) %>%
   .[, c("y", "x") := tstrsplit(pixel, "_", type.convert=TRUE)]
 
-fwrite(pixels, file = paste0(outDir,"pixels.txt"), sep = "\t", na = "NA", quote = F, col.names = F)
+# fwrite(pixels, file = paste0(outDir,"pixels.txt"), sep = "\t", na = "NA", quote = F, col.names = F)
 
 slices <- pixels[,y] %>% unique
 
-lapply(c("EUR", "CSA", "AFR"), function(anc) {
+anc <- "EUR"
   
   ids <- paste0(dataDir,"sampleList_singleIDs_",anc,".txt") %>%
     fread
@@ -83,15 +124,15 @@ lapply(c("EUR", "CSA", "AFR"), function(anc) {
   print(paste(nrow(phenoOut[IID %in% ids[,IID]]), "individuals with phenotype data."))
   
 
+## Sensitivity analyses:
+## 1. add smoking as a covariate
+## 2. exclude individuals who underwent glaucoma surgery
+
 #write covariate file- both with single, and double IDs
 fwrite(covsOut[IID %in% ids[,IID]], file = paste0(outDir,"covariates_doubleIDs_",anc,".txt"), sep = "\t", na = "NA", quote = F)
 
 
-# fwrite(phenoOut[IID %in% ids[,IID]], file = paste0(outDir,"FPCphenotypes_doubleIDs_",anc,".txt"), sep = "\t", na = "NA", quote = F)
 
-## Sensitivity analyses:
-## 1. add smoking as a covariate
-## 2. exclude individuals who underwent surgery
 
 sapply(slices, function(idx) {
 
